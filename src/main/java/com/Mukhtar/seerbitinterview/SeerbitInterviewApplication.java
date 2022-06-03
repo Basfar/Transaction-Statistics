8ba9ba6f8ef8aeb2ca761c6e2920ae5d06f8bbfa
basfar <mukhtaronif@gmail.com>
2022-06-03 21:14:46 +0100

    Initial commit

---
+++ src/main/java/com/Mukhtar/seerbitinterview/SeerbitInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.Mukhtar.seerbitinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerbitInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeerbitInterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mukhtar/seerbitinterview/SeerbitInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mukhtar.seerbitinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerbitInterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
